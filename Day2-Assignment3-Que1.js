66aa23129f9b9fba034834db7bf19becf9d5ae2b
AkshayS-253 <52171049+AkshayS-253@users.noreply.github.com>
2020-07-16 13:37:29 +0530

    Add files via upload

---
+++ Day2-Assignment3-Que1.js
@@ -0,0 +1,6 @@
+var name = prompt("Enter your name"); // user input name 
+console.log("Name = "+name)
+
+var surname= prompt("Enter your surname") // user input surname
+console.log("Surname = "+surname)
+
